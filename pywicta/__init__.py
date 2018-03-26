4383be9b2fea7bf7dea218fcd655416ff059537e
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-03-27 00:02:11 +0200

    Fix a bug.

--- pywicta/__init__.py
+++ pywicta/__init__.py
@@ -38,13 +38,14 @@
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 
-__version__ = '0.1.dev21'
+__version__ = '0.1.dev22'
 
 def get_version():
     return __version__
 
-from . import benchmark
-from . import denoising
-from . import image
-from . import io
-from . import optimization
+# The following lines are temporary commented to avoid BUG#2 (c.f. BUGS.md)
+#from . import benchmark
+#from . import denoising
+#from . import image
+#from . import io
+#from . import optimization
