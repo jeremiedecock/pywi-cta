1095942fcbdae50b21c9359c6d7ac17e9abb513a
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-04-08 17:11:22 +0200

    Misc updates.

--- utils/copy_fits_to_ramdisk.sh
+++ utils/copy_fits_to_ramdisk.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-cp $(find /home/jdecock/data/grid_prod3b_north/fits/lst/gamma/  -type f -name "*.fits" | head -n 10000) /dev/shm/.jd/fits/gamma/
-cp $(find /home/jdecock/data/grid_prod3b_north/fits/lst/proton/ -type f -name "*.fits" | head -n 10000) /dev/shm/.jd/fits/proton/
+cp -a ~/data/grid_prod3b_north/fits/lst/gamma /dev/shm/.jd/lstcam/
+#cp $(find /home/jdecock/data/grid_prod3b_north/fits/lst/gamma/  -type f -name "*.fits" | head -n 10000) /dev/shm/.jd/fits/gamma/
+#cp $(find /home/jdecock/data/grid_prod3b_north/fits/lst/proton/ -type f -name "*.fits" | head -n 10000) /dev/shm/.jd/fits/proton/
