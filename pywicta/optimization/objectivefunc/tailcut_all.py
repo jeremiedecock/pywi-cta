e52db22cf46d77df802d3ed4677fb7442308d641
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-04-10 13:20:29 +0200

    Hack to prevent a fail on an image to cause an fail on the tested thresholds.

--- pywicta/optimization/objectivefunc/tailcut_all.py
+++ pywicta/optimization/objectivefunc/tailcut_all.py
@@ -28,6 +28,9 @@ from pywicta.denoising.tailcut import Tailcut
 from pywicta.benchmark import assess
 from pywicta.benchmark.assess import norm_angle_diff
 
+import traceback
+import sys
+
 
 # OPTIMIZER ##################################################################
 
@@ -67,7 +70,7 @@ class ObjectiveFunction:
     def __call__(self, threshold_list):
         self.call_number += 1
 
-        aggregated_score = []
+        aggregated_scores = []
 
         try:
             high_threshold = float(threshold_list[0])
@@ -137,7 +140,8 @@ class ObjectiveFunction:
                 else:
                     # The cleaning algorithm failed to clean this image
                     # TODO: add a penalty
-                    scores = []
+                    NUM_METRICS = 29
+                    scores = [float('nan') for score in range(NUM_METRICS)]      # TODO: AVOID THIS HARD CODED VALUE !!!
 
                 # WORKAROUND
 
@@ -163,20 +167,28 @@ class ObjectiveFunction:
 
             # Compute the mean
             if self.aggregation_method == "mean":
-                aggregated_score = np.nanmean(score_array, axis=0)
+                aggregated_scores = np.nanmean(score_array, axis=0)
             elif self.aggregation_method == "median":
-                aggregated_score = np.nanmedian(score_array, axis=0)
+                aggregated_scores = np.nanmedian(score_array, axis=0)
             else:
                 raise ValueError("Unknown value for aggregation_method: {}".format(self.aggregation_method))
 
             # TODO: save results in a JSON file (?)
-            print(algo_params_var, aggregated_score, self.aggregation_method)
+            print(algo_params_var, aggregated_scores, self.aggregation_method)
         except Exception as e:
-            print(e)
+            print("ERROR: error with thresholds", threshold_list, "(aborted)", e)
+            # The following line print the full trackback
+            traceback.print_tb(e.__traceback__, file=sys.stdout)
+            sys.exit(1)
 
-        self.aggregated_score_list.append([float(score) for score in aggregated_score])
+        self.aggregated_score_list.append([float(score) for score in aggregated_scores])
 
-        return float(aggregated_score[-1])    # TODO: use name instead of index...
+        try:
+            aggregated_score = float(aggregated_scores[-1])    # TODO: use name instead of index...
+        except:
+            aggregated_score = float('nan')                    # TODO: use name instead of index...
+
+        return aggregated_score
 
 
 if __name__ == "__main__":
