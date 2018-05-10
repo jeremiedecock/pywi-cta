4eb3c24fa7b9385a27dad194e347e427d2af754a
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-05-10 23:11:36 +0200

    Update the version number.

--- pywicta/__init__.py
+++ pywicta/__init__.py
@@ -38,7 +38,7 @@
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 
-__version__ = '0.1.dev31'
+__version__ = '0.1.dev32'
 
 def get_version():
     return __version__
